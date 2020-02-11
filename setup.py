d548765faae120977902c7842e57bde408abc048
Jérémy DEMANGE <j@lachainemedias.com>
2020-02-11 11:12:16 +0100

    Version 0.0.12.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='iPyCat',
-    version='0.0.11',
+    version='0.0.12',
     scripts=['ipycat'],
     author="Jérémy DEMANGE",
     author_email="jeremy.demange.mail@gmail.com",
--- utils/stop_words.py
+++ utils/stop_words.py
@@ -1,14 +1,16 @@
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
  
-def stopWords():
+def stopWords(fichier, langue):
     data = "Bonjour à tous, j'espère que vous allez bien !"
-    stopWords = set(stopwords.words('french'))
+    dataWithStop = ""
+    stopWords = set(stopwords.words(langue))
     words = word_tokenize(data)
     wordsFiltered = []
 
     for w in words:
         if w not in stopWords:
+            dataWithStop += w + " "
             wordsFiltered.append(w)
 
-    print(wordsFiltered)
+    print(dataWithStop)
